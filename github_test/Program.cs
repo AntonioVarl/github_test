7307eb77cbceec9d8fd4194e9fc69328ec1f352e
anton_varlamov <anton_varlamov3@inbox.ru>
2023-12-20 13:25:02 +0600

    feat: add product of numbers

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -6,16 +6,17 @@
         {
             Console.WriteLine("Введите первое число:");
             var firstNumber = Convert.ToDouble(Console.ReadLine());
-        
+
             Console.WriteLine($"Выберите команду: \n" +
                               $"1. Сложить числа \n" +
-                              $"2. Вычесть числа \n");
+                              $"2. Вычесть числа \n" +
+                              $"3. Умножить числа \n");
 
             var choice = Convert.ToInt32(Console.ReadLine());
-        
+
             Console.WriteLine("Введите второе число:");
             var secondNumber = Convert.ToDouble(Console.ReadLine());
-            
+
             switch (choice)
             {
                 case 1:
@@ -28,6 +29,12 @@
                 {
                     var difference = firstNumber - secondNumber;
                     Console.WriteLine("Результат вычитания " + difference);
+                    break;
+                }
+                case 3:
+                {
+                    var product = firstNumber * secondNumber;
+                    Console.WriteLine("Результат умножения " + product);
                     break;
                 }
             }
